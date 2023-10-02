8138c6b0b4ef8082b4562e8ab39520175cd39398
Benzel <benawhiting@gmail.com>
2023-10-02 15:32:44 -0700

    Create loop for calculating scores

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -5,19 +5,39 @@ int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
-
-decimal sophiaScore;
-
-foreach (int score in sophiaScores)
-{
-  sophiaSum += score;
-}
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int[10];
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+
+foreach (string name in studentNames)
+{
+  string currentStudent = name;
+  if (currentStudent == "Sophia")
+    studentScores = sophiaScores;
+
+  else if (currentStudent == "Andrew")
+    studentScores = andrewScores;
+
+  else if (currentStudent == "Emma")
+    studentScores = emmaScores;
+
+  else if (currentStudent == "Logan")
+    studentScores = loganScores;
+
+  int studentSum = 0;
+
+  decimal finalScore;
+
+  foreach (int score in studentScores)
+  {
+    studentSum += score;
+  }
+
+  finalScore = (decimal)studentSum / currentAssignments;
+
+  Console.WriteLine($"{name}:\t\t" + finalScore + "\tA-");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
